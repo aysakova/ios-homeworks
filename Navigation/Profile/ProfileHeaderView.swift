47be0019866da7aabf9e4ab22f7512b083845b5c
Aysa <aysakova@naver.com>
2021-08-14 00:11:16 +0900

    UI build in code complete

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -10,43 +10,70 @@ import UIKit
 
 class ProfileHeaderView: UIView {
     
-    @IBOutlet weak var avatarImage: UIImageView! {
-        didSet {
-            avatarImage.layer.cornerRadius = avatarImage.frame.width / 2
-            avatarImage.layer.borderWidth = 3
-            avatarImage.layer.borderColor = UIColor.white.cgColor
-        }
-    }
-    @IBOutlet weak var nameLabel: UILabel! {
-        didSet {
-            nameLabel.font = UIFont.boldSystemFont(ofSize: 18)
-        }
-    }
-    @IBOutlet weak var statusTextField: UITextField! {
-        didSet {
-            statusTextField.textColor = UIColor.gray
-            statusTextField.borderStyle = .none
-            statusTextField.font = UIFont.systemFont(ofSize: 14, weight: .regular)
-        }
-    }
-    @IBOutlet weak var showButton: UIButton! {
-        didSet {
-            showButton.backgroundColor = .systemBlue
-            showButton.layer.cornerRadius = 14
-            showButton.layer.shadowOffset = CGSize(width: 4, height: 4)
-            showButton.layer.shadowRadius = 4
-            showButton.layer.shadowColor = UIColor.black.cgColor
-            showButton.layer.shadowOpacity = 0.7
-            showButton.addTarget(self, action: #selector(handleTap), for: .touchUpInside)
+    lazy var avatarImage: UIImageView = {
+        let avatarImage = UIImageView()
+        avatarImage.image = UIImage(named: "roxy")
+        avatarImage.layer.cornerRadius = avatarImage.frame.width / 2
+        avatarImage.layer.borderWidth = 3
+        avatarImage.layer.masksToBounds = true
+        avatarImage.layer.borderColor = UIColor.white.cgColor
+       return avatarImage
+     }()
+    
+    
+    lazy var nameLabel: UILabel = {
+        let nameLabel = UILabel()
+        nameLabel.text = "Badass cat"
+        nameLabel.textColor = .black
+        nameLabel.font = UIFont.boldSystemFont(ofSize: 18)
+        
+        return nameLabel
+    }()
+    
+    lazy var statusTextField: UITextField = {
+        let statusTextField = UITextField()
+        statusTextField.textColor = UIColor.gray
+        statusTextField.borderStyle = .none
+        statusTextField.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        statusTextField.placeholder = "Write something here..."
 
-        }
+        return statusTextField
+    }()
+    
+    lazy var showButton: UIButton = {
+        let showButton = UIButton()
+        showButton.backgroundColor = .systemBlue
+        showButton.setTitle("Show status", for: .normal)
+        
+        showButton.layer.cornerRadius = 14
+        showButton.layer.shadowOffset = CGSize(width: 4, height: 4)
+        showButton.layer.shadowRadius = 4
+        showButton.layer.shadowColor = UIColor.black.cgColor
+        showButton.layer.shadowOpacity = 0.7
+        showButton.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        return showButton
+    }()
+    
+    override init(frame:CGRect) {
+        super.init(frame: frame)
+        createSubviews()
     }
-    @objc func handleTap() {
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        createSubviews()
+    }
+    
+    private func createSubviews() {
+        addSubview(avatarImage)
+        addSubview(nameLabel)
+        addSubview(statusTextField)
+        addSubview(showButton)
+    }
+    
+    @objc func buttonPressed() {
         print(self.statusTextField.text ?? "no input")
         }
-    
-    
-    
 }
 
 
--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -25,15 +25,54 @@ class ProfileVIewController: UIViewController {
         
     override func viewDidLoad() {
         self.view.backgroundColor = .lightGray
-        if let headerView = Bundle.main.loadNibNamed("ProfileHeaderView", owner: nil, options: nil)?.first as? ProfileHeaderView {
             view.addSubview(headerView)
-
-        }
     }
 
 
     
     override func viewWillLayoutSubviews() {
         headerView.frame = view.frame
+        setupLayout()
+
+    }
+    
+    private func setupLayout() {
+        let avatar = headerView.avatarImage
+        avatar.layer.cornerRadius = avatar.frame.width / 2
+        avatar.translatesAutoresizingMaskIntoConstraints = false
+        
+        let label = headerView.nameLabel
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        let text = headerView.statusTextField
+        text.translatesAutoresizingMaskIntoConstraints = false
+        
+        let button = headerView.showButton
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        let constraint = view.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            avatar.topAnchor.constraint(equalTo: constraint.topAnchor, constant: 16),
+            avatar.leadingAnchor.constraint(equalTo: constraint.leadingAnchor, constant: 16),
+            avatar.widthAnchor.constraint(equalToConstant: 120),
+            avatar.heightAnchor.constraint(equalToConstant: 120),
+            
+            label.widthAnchor.constraint(equalToConstant: 200),
+            label.heightAnchor.constraint(equalToConstant: 30),
+            label.topAnchor.constraint(equalTo: constraint.topAnchor, constant: 27),
+            label.leadingAnchor.constraint(equalTo: constraint.leadingAnchor, constant: 150),
+            label.trailingAnchor.constraint(equalTo: constraint.trailingAnchor, constant: 16),
+
+            text.bottomAnchor.constraint(equalTo: button.topAnchor, constant: -34),
+            text.leadingAnchor.constraint(equalTo: constraint.leadingAnchor, constant: 150),
+            text.trailingAnchor.constraint(equalTo: constraint.trailingAnchor, constant: 16),
+            
+            button.heightAnchor.constraint(equalToConstant: 50),
+            button.widthAnchor.constraint(equalToConstant: button.frame.width),
+            button.topAnchor.constraint(equalTo: avatar.bottomAnchor, constant: 16),
+            button.leadingAnchor.constraint(equalTo: constraint.leadingAnchor, constant: 16),
+            button.trailingAnchor.constraint(equalTo: constraint.trailingAnchor, constant: -16)
+         ])
     }
 }
